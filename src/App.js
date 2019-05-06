f25ea1baaea6e6dedc24c1a56e807a79d21082c5
Matt Cool <matthewbcool@gmail.com>
2019-05-06 16:29:41 -0500

    layout with cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
 
 import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import LinearProgress from '@material-ui/core/LinearProgress'
 import './App.css'
 
 export default class App extends Component {
+  state = {
+    progressValue: 0
+  }
   render() {
     return (
       <div className='App'>
+        <CssBaseline />
         <header className='App-header'>
           <h1>AmILegal</h1>
-          <Button variant='contained' color='primary'>
-            test{' '}
-          </Button>
         </header>
+        <Grid container direction='column' justify='center' alignItems='center'>
+          <Grid item xs={12}>
+            <LinearProgress variant='indeterminate' value={0} />
+          </Grid>
+          <Grid item xs={12}>
+            <Card raised className='questions-card' />
+          </Grid>
+          <Grid item xs={12}>
+            <Card raised className='results-card' />
+          </Grid>
+        </Grid>
       </div>
     )
   }
