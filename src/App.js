0bbd0de48d39a0c0c11074fcda9637a44e73048c
Matt Cool <matthewbcool@gmail.com>
2019-05-06 22:57:39 -0500

    basic card with input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
-
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
 import LinearProgress from '@material-ui/core/LinearProgress'
+import MobileStepper from '@material-ui/core/MobileStepper'
+import CardContent from '@material-ui/core/CardContent'
+import Input from '@material-ui/core/Input'
+
 import './App.css'
 
 export default class App extends Component {
   state = {
-    progressValue: 0
+    progressValue: 0,
+    activeStep: 0,
+    slectedTime: 1200
   }
+
   render() {
+    const stepperData = [
+      { question: 'When was your sign-in?', component: <Button>YES</Button> },
+      { question: 'Station code you signed in from?' },
+      { question: 'Is this an international trip?' },
+      { question: 'What kind of international trip is this?' },
+      { question: "You're illegal at:" }
+    ]
+
+    const handleTimeChange = () => {
+      console.log('change the time')
+    }
     return (
       <div className='App'>
         <CssBaseline />
@@ -25,12 +42,18 @@ export default class App extends Component {
         />
         <Grid container direction='column' justify='center' alignItems='center'>
           <Grid item xs={12}>
-            <Card raised className='questions-card' />
+            <Card raised className='questions-card'>
+              <h1> {stepperData[this.state.activeStep].question}</h1>
+              <CardContent>
+                <Input defaultValue={1200} />
+              </CardContent>
+            </Card>
           </Grid>
           <Grid item xs={12}>
             <Card raised className='results-card' />
           </Grid>
         </Grid>
+        <footer className='footer'>this is the footer</footer>{' '}
       </div>
     )
   }
