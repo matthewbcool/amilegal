321ad791d72666fdd23ab89e0f407ee1c2c7e13f
Jason White <jasonmichaelwhite@gmail.com>
2019-05-09 00:59:01 -0400

    CHANGED: TimePicker syntax

--- src/TimePicker.js
+++ src/TimePicker.js
@@ -1,148 +1,91 @@
-import React from 'react'
-import InputLabel from '@material-ui/core/InputLabel'
-import MenuItem from '@material-ui/core/MenuItem'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import AccessTime from '@material-ui/icons/AccessTime'
-import './App.css'
+import React, { Component } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import AccessTime from "@material-ui/icons/AccessTime";
+import "./App.css";
 
-class TimePicker extends React.Component {
-  render() {
-    return (
-      <form className='time-picker-form' autoComplete='off'>
-        <FormControl>
-          <InputLabel
-            style={{ padding: '0px 0px 2px 0px', margin: '5px 0px 0px 0px' }}
-            htmlFor='hour'>
-            Hour
-          </InputLabel>
-          <Select
-            style={{
-              fontSize: '2rem',
-              padding: '0px 0px 5px 0px',
-              width: '70px'
-            }}
-            value={this.props.hour}
-            className='time-picker-select'
-            onChange={this.props.handleChange}
-            inputProps={{
-              name: 'hour',
-              id: 'hour'
-            }}>
-            <MenuItem value=''>Hour</MenuItem>
-            <MenuItem value={1}>01</MenuItem>
-            <MenuItem value={2}>02</MenuItem>
-            <MenuItem value={3}>03</MenuItem>
-            <MenuItem value={4}>04</MenuItem>
-            <MenuItem value={5}>05</MenuItem>
-            <MenuItem value={6}>06</MenuItem>
-            <MenuItem value={7}>07</MenuItem>
-            <MenuItem value={8}>08</MenuItem>
-            <MenuItem value={9}>09</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={11}>11</MenuItem>
-            <MenuItem value={12}>12</MenuItem>
-            <MenuItem value={13}>13</MenuItem>
-            <MenuItem value={14}>14</MenuItem>
-            <MenuItem value={15}>15</MenuItem>
-            <MenuItem value={16}>16</MenuItem>
-            <MenuItem value={17}>17</MenuItem>
-            <MenuItem value={18}>18</MenuItem>
-            <MenuItem value={19}>19</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-            <MenuItem value={21}>21</MenuItem>
-            <MenuItem value={22}>22</MenuItem>
-            <MenuItem value={23}>23</MenuItem>
-            <MenuItem value={24}>00</MenuItem>
-          </Select>
-        </FormControl>
-        <h1 style={{ margin: '0px 10px 0px 10px' }}>:</h1>
-        <FormControl>
-          <InputLabel
-            style={{ padding: '0px 0px 2px 0px', margin: '5px 0px 0px 0px' }}
-            htmlFor='minute'>
-            Minutes
-          </InputLabel>
-          <Select
-            style={{
-              fontSize: '2rem',
-              padding: '0px 0px 5px 0px',
-              width: '70px'
-            }}
-            value={this.props.minute}
-            className='time-picker-select'
-            onChange={this.props.handleChange}
-            inputProps={{
-              name: 'minute',
-              id: 'minute'
-            }}>
-            <MenuItem value=''>Minutes</MenuItem>
-            <MenuItem value={1}>01</MenuItem>
-            <MenuItem value={2}>02</MenuItem>
-            <MenuItem value={3}>03</MenuItem>
-            <MenuItem value={4}>04</MenuItem>
-            <MenuItem value={5}>05</MenuItem>
-            <MenuItem value={6}>06</MenuItem>
-            <MenuItem value={7}>07</MenuItem>
-            <MenuItem value={8}>08</MenuItem>
-            <MenuItem value={9}>09</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={11}>11</MenuItem>
-            <MenuItem value={12}>12</MenuItem>
-            <MenuItem value={13}>13</MenuItem>
-            <MenuItem value={14}>14</MenuItem>
-            <MenuItem value={15}>15</MenuItem>
-            <MenuItem value={16}>16</MenuItem>
-            <MenuItem value={17}>17</MenuItem>
-            <MenuItem value={18}>18</MenuItem>
-            <MenuItem value={19}>19</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-            <MenuItem value={21}>21</MenuItem>
-            <MenuItem value={22}>22 </MenuItem>
-            <MenuItem value={23}>23 </MenuItem>
-            <MenuItem value={24}>24</MenuItem>
-            <MenuItem value={25}>25</MenuItem>
-            <MenuItem value={26}>26</MenuItem>
-            <MenuItem value={27}>27</MenuItem>
-            <MenuItem value={28}>28</MenuItem>
-            <MenuItem value={29}>29</MenuItem>
-            <MenuItem value={30}>30</MenuItem>
-            <MenuItem value={31}>31</MenuItem>
-            <MenuItem value={32}>32</MenuItem>
-            <MenuItem value={33}>33</MenuItem>
-            <MenuItem value={34}>34</MenuItem>
-            <MenuItem value={35}>35</MenuItem>
-            <MenuItem value={36}>36</MenuItem>
-            <MenuItem value={37}>37</MenuItem>
-            <MenuItem value={38}>38</MenuItem>
-            <MenuItem value={39}>39</MenuItem>
-            <MenuItem value={40}>40</MenuItem>
-            <MenuItem value={41}>41</MenuItem>
-            <MenuItem value={42}>42</MenuItem>
-            <MenuItem value={43}>43</MenuItem>
-            <MenuItem value={44}>44</MenuItem>
-            <MenuItem value={45}>45</MenuItem>
-            <MenuItem value={46}>46</MenuItem>
-            <MenuItem value={47}>47</MenuItem>
-            <MenuItem value={48}>48</MenuItem>
-            <MenuItem value={49}>49</MenuItem>
-            <MenuItem value={50}>50</MenuItem>
-            <MenuItem value={51}>51</MenuItem>
-            <MenuItem value={52}>52</MenuItem>
-            <MenuItem value={53}>53</MenuItem>
-            <MenuItem value={54}>54</MenuItem>
-            <MenuItem value={55}>55</MenuItem>
-            <MenuItem value={56}>56</MenuItem>
-            <MenuItem value={57}>57</MenuItem>
-            <MenuItem value={58}>58</MenuItem>
-            <MenuItem value={59}>59</MenuItem>
-            <MenuItem value={0}>00</MenuItem>
-          </Select>
-        </FormControl>
-        <AccessTime fontSize='large' />
-      </form>
-    )
+const getTimes = (placeholder, min, max) => {
+  const items = [];
+  items.push(
+    <MenuItem key={`${placeholder}`} value="">
+      {placeholder}
+    </MenuItem>
+  );
+  for (let i = min; i <= max; i++) {
+    if (i < 10)
+      items.push(
+        <MenuItem key={`${placeholder}-${i}`} value={i}>{`0${i}`}</MenuItem>
+      );
+    else if (i < max)
+      items.push(
+        <MenuItem key={`${placeholder}-${i}`} value={i}>
+          {i}
+        </MenuItem>
+      );
+    else
+      items.push(
+        <MenuItem key={`${placeholder}-${i}`} value={i}>
+          00
+        </MenuItem>
+      );
   }
-}
+  return items;
+};
 
-export default TimePicker
+const TimePicker = ({ hour, minute, handleChange }) => (
+  <form className="time-picker-form" autoComplete="off">
+    <FormControl>
+      <InputLabel
+        style={{ padding: "0px 0px 2px 0px", margin: "5px 0px 0px 0px" }}
+        htmlFor="hour"
+      >
+        Hour
+      </InputLabel>
+      <Select
+        style={{
+          fontSize: "2rem",
+          padding: "0px 0px 5px 0px",
+          width: "70px"
+        }}
+        value={hour}
+        className="time-picker-select"
+        onChange={handleChange}
+        inputProps={{
+          name: "hour",
+          id: "hour"
+        }}
+      >
+        {getTimes("Hour", 1, 24)}
+      </Select>
+    </FormControl>
+    <h1 style={{ margin: "0px 10px 0px 10px" }}>:</h1>
+    <FormControl>
+      <InputLabel
+        style={{ padding: "0px 0px 2px 0px", margin: "5px 0px 0px 0px" }}
+        htmlFor="minute"
+      >
+        Minutes
+      </InputLabel>
+      <Select
+        style={{
+          fontSize: "2rem",
+          padding: "0px 0px 5px 0px",
+          width: "70px"
+        }}
+        value={minute}
+        className="time-picker-select"
+        onChange={handleChange}
+        inputProps={{
+          name: "minute",
+          id: "minute"
+        }}
+      >
+        {getTimes("Minutes", 1, 60)}
+      </Select>
+    </FormControl>
+    <AccessTime fontSize="large" />
+  </form>
+);
+export default TimePicker;
