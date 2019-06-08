0a8ac9adc001df340941919b7a62eab12944efe1
Matt Cool <matthewbcool@gmail.com>
2019-06-08 07:32:22 -0500

    querystringify package edit  security warning

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+afterEach(cleanup)
+
+it('app matches snapshot', () => {
+  const { asFragment } = render(<App />)
+  expect(asFragment()).toMatchSnapshot()
+})
