f209d803eecc119d8f2ddf0f4dd5b02f61b56db9
Matt Cool <matthewbcool@gmail.com>
2019-06-26 10:55:23 -0500

    bugfix: signin, deleted unused package update minor version

--- src/App.js
+++ src/App.js
@@ -460,7 +460,7 @@ export default class App extends Component {
           </Grid>
         </Grid>
         <footer className='footer'>
-          <p>2.1.2 </p>© 2019 Cool Dev Labs
+          <p>2.2.0 </p>© 2019 Cool Dev Labs
           <InfoDialog />
         </footer>
       </div>
