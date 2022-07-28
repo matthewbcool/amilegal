ad1b9d1a582477eb746ac25144ed0ddb0ff93a07
Matt <matthewbcool@gmail.com>
2022-07-28 13:32:36 -0700

    remove ad

--- src/components/BraveAd.js
+++ src/components/BraveAd.js
@@ -6,29 +6,7 @@ import banner from '../300x100-standard.png'
 export default function BraveAd() {
   return (
     <div>
-      <Paper
-        className='brave-ad'
-        style={{ backgroundColor: '#282c34', borderRadius: '0px' }}>
-        <a href='https://brave.com/aam392'>
-          <Typography
-            align='center'
-            variant='caption'
-            display='block'
-            color='inherit'
-            style={{ paddingBottom: '5px', margin: '0 2em' }}>
-            Support AAmilegal for free by downloading and using Brave.
-          </Typography>
-          <img alt='download brave banner ad' src={banner} />
-          <Typography
-            align='center'
-            variant='caption'
-            display='block'
-            color='inherit'
-            style={{ paddingBottom: '5px', margin: '0 2em' }}>
-            Brave is a web browser that blocks ads and privacy trackers and pays you for your attention. Watch youtube ad free, search the web freely without your data being used.
-          </Typography>
-        </a>
-      </Paper>
+    
     </div>
   )
 }
